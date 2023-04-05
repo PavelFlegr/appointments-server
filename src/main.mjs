109fb7be4426cde6becbf37be71f282140756a58
pavel <pavelflegr@gmail.com>
2023-04-05 18:11:10 +0200

    make cancel reservation link bigger

--- src/main.mjs
+++ src/main.mjs
@@ -96,6 +96,21 @@ fastify.post('/login', {
 
 })
 
+const style = `box-shadow:inset 0px 1px 0px 0px #f5978e;
+	background:linear-gradient(to bottom, #f24537 5%, #c62d1f 100%);
+	background-color:#f24537;
+	border-radius:6px;
+	border:1px solid #d02718;
+	display:inline-block;
+	cursor:pointer;
+	color:#ffffff;
+	font-family:Arial;
+	font-size:15px;
+	font-weight:bold;
+	padding:6px 24px;
+	text-decoration:none;
+	text-shadow:0px 1px 0px #810e05;`
+
 fastify.post('/reservation', {
     async handler (request) {
         // TODO: use aggregation
@@ -104,7 +119,7 @@ fastify.post('/reservation', {
         const appointmentOwner = await userService.getUser(appointment.userId)
         const time = dayjs(reservation.start).tz(reservation.timezone).format("DD. MM. YYYY HH:mm")
         emailService.sendEmail("Reservation Created",
-            `Your reservation for ${time} is registered. You can cancel it by clicking <a href="${Config.appHost}/cancel/${reservation.id}">here</a><div>${appointment.instructions}</div>`, reservation.email, appointmentOwner.email)
+            `Your reservation for ${time} is registered. You can cancel it by clicking <a style="${style}" href="${Config.appHost}/cancel/${reservation.id}">here</a><div style="min-height: 300px">${appointment.instructions}</div>`, reservation.email, appointmentOwner.email)
             .catch(e => console.error(`sending email failed: ${JSON.stringify(e)}`))
 
         return true
